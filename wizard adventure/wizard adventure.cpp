8a4125ce8b9a21a7b16ac49a44be815ef0513238
AbelkAntony <abelkantony98@gmail.com>
2023-01-30 14:01:17 +0530

    Update wizard adventure.cpp

--- wizard adventure/wizard adventure.cpp
+++ wizard adventure/wizard adventure.cpp
@@ -1,23 +1,26 @@
 #include <iostream>
 using namespace std;
+
 class Character
 {
 private:
 	string name;
 	int maxHealth;
-	int health;
+	int currentHealth;
 	int damagePoint;
 	int defence;
 	int healing;
+	int agility;
 public:
-	Character(string name, int health, int damagePoint, int defence, int healing)
+	Character(string name, int health, int damagePoint, int defence, int healing, int agility)
 	{
 		this->name = name;
 		this->maxHealth = health;
-		this->health = health;
+		this->currentHealth = health;
 		this->damagePoint = damagePoint;
 		this->defence = defence;
 		this->healing = healing;
+		this->agility = agility;
 	}
 	virtual string GetName()
 	{
@@ -25,19 +28,23 @@ public:
 	}
 	int GetHealth()
 	{
-		return health;
+		return currentHealth;
 	}
 	int GetDamage()
 	{
 		return damagePoint;
 	}
+	int GetAgility()
+	{
+		return agility;
+	}
 	virtual void TakeDamage(int _damage)
 	{
 		int damage = (_damage - defence);
-		health = health - damage;
+		currentHealth = currentHealth - damage;
 		cout << "\nCause damage : " << _damage << endl;
 	}
-	void MainStatus()
+	void DisplayAllStatus()
 	{
 		cout << "NAME : " << name << endl;
 		cout << "HEALTH : " << maxHealth << endl;
@@ -46,41 +53,43 @@ public:
 		cout << "HEALING POINT : " << healing << endl;
 
 	}
-	void Status()
+	void DisplayHealthStatus()
 	{
-		cout << endl << name << "'s health : " << health;
+		cout << endl << name << "'s health : " << currentHealth;
 	}
 	virtual void Heal()
 	{
-		if (maxHealth == health)
+		if (maxHealth == currentHealth)
 			cout << endl << name << "'s health is already full\n";
-		else if (health + healing > maxHealth)
+		else if (currentHealth + healing > maxHealth)
 		{
-			int tempheal;
-			tempheal = (maxHealth - health);
-			health += tempheal;
-			cout << "/n" << name << "'s health is added by " << tempheal;
+			int tempHeal;
+			tempHeal = (maxHealth - currentHealth);
+			currentHealth += tempHeal;
+			cout << "/n" << name << "'s health is added by " << tempHeal;
 		}
 		else
 		{
-			health += healing;
+			currentHealth += healing;
 			cout << "\n" << name << "'s health is added by " << healing;
 		}
 	}
-	virtual int Run()
+	virtual int RunAwayFromAttack(Character* character1, Character* character2)
 	{
+		int totalAgility = character1->GetAgility() + character2->GetAgility();
+		int percentageOfAgility = (character1->GetAgility() * 100) / totalAgility;
 		srand(time(0));
-		int chance = 0;
-		chance = rand() % 2;
-		if (chance == 0)
+		int chance;
+		chance = rand() % 100;
+		if (chance <= percentageOfAgility)
 		{
-			cout << name << " TRY'S TO RAN AWAY SORRY BUT CAN'T RAN AWAY \n";
-			return chance;
+			cout << name << " RUN AWAY\n";
+			return 1;
 		}
 		else
 		{
-			cout << name << " RUN AWAY\n";
-			return chance;
+			cout << name << " TRY'S TO RAN AWAY SORRY BUT CAN'T RAN AWAY \n";
+			return 0;
 		}
 	}
 };
@@ -88,53 +97,59 @@ public:
 class Wizard : public Character
 {
 public:
-	Wizard(string name) :Character(name, 100, 35, 10, 50)
+	Wizard(string name) :Character(name, 100, 35, 10, 50, 10)
 	{}
 	~Wizard() {}
 };
 
-class Enemy : public Character
+class Dragon : public Character
 {
 public:
-	Enemy(string name, int health, int damagePoint, int defence, int healing) :Character(name, health, damagePoint, defence, healing)
+	Dragon() :Character("Dragon", 100, 25, 10, 40, 15)
 	{}
-	~Enemy() {}
+	~Dragon() {}
 };
 
 class GamePlay
 {
 private:
 	//variables
+	int run = 0;
 	int round;
 	int damage;
 	Character* player;
 	Character* enemy;
 	string name;
 	//function
-	void GameTitle()
+	void DisplayGameTitle()
 	{
 		cout << "\n				    	WIZARD ADVENTURE\n\n";
 	}
-	void CharacterCreation()
+	void EnemyCharacterCreation()
+	{
+		enemy = new Dragon();
+	}
+	void PlayerCharacterCreation()
 	{
 		cout << "ENTER YOUR NAME : ";
 		cin >> name;
 		player = new Wizard(name);
 	}
-	void MainStatus()
+	void DisplayAllDetailsOfCharacters()
 	{
 		cout << "\nPLAYER\n";
-		player->MainStatus();
+		player->DisplayAllStatus();
 		cout << "\nENEMY\n";
-		enemy->MainStatus();
+		enemy->DisplayAllStatus();
 	}
-	void Status()
+	void HealthStatus()
 	{
-		player->Status();
-		enemy->Status();
+		player->DisplayHealthStatus();
+		enemy->DisplayHealthStatus();
 	}
-	void EnemyGameRound()
+	int EnemyGameRound()
 	{
+		cout << endl << endl << enemy->GetName() << "'s Turn\n";
 		int health20Percentage = (enemy->GetHealth()) * .2;
 		int health10Percentage = (enemy->GetHealth()) * .1;
 		if (enemy->GetHealth() > health20Percentage)
@@ -145,24 +160,26 @@ private:
 		else if (enemy->GetHealth() < health20Percentage && enemy->GetHealth() > health10Percentage)
 		{
 			enemy->Heal();
-			damage = enemy->GetDamage();
-			player->TakeDamage(damage);
 		}
 		else if (enemy->GetHealth() < health10Percentage)
 		{
-			int run = enemy->Run();
+			run = enemy->RunAwayFromAttack(enemy, player);
 			if (run == 0)
 			{
 			}
 			else if (run == 1)
 			{
 				cout << player->GetName() << " wins";
+				return run;
 			}
 		}
+		HealthStatus();
+		return run;
 	}
 
-	void PlayerGameRound()
+	int PlayerGameRound()
 	{
+		cout << endl << player->GetName() << "'s Turn choose your option\n";
 		int option;
 		cout << "\n1. Attack\n2. Heal\n3. Run\nEnter your option : ";
 		cin >> option;
@@ -177,13 +194,13 @@ private:
 			break;
 		case 3:
 		{
-			int run = player->Run();
+			run = player->RunAwayFromAttack(player, enemy);
 			if (run == 0)
 				break;
 			else if (run == 1)
 			{
 				cout << player->GetName() << " lose";
-				exit(0);
+				return run;
 			}
 		}
 		default:
@@ -191,33 +208,95 @@ private:
 			damage = player->GetDamage();
 			enemy->TakeDamage(damage);
 		}
+		HealthStatus();
+		return run;
 	}
-	void PlayGame()
+	int CharacterTurnDecider()
 	{
-		//enemy created
-		enemy = new Enemy("Dragon", 100, 25, 10, 40);
-		MainStatus();
+		if (enemy->GetAgility() < player->GetAgility())
+		{
+			run = PlayerGameRound();
+			if (run == 1)
+				return run;
+			run = EnemyGameRound();
+			if (run == 1)
+				return run;
+		}
+		else
+		{
+			run = EnemyGameRound();
+			if (run == 1)
+				return run;
+			run = PlayerGameRound();
+			if (run == 1)
+				return run;
+		}
+		return run;
+	}
+	int PlayGame()
+	{
+		DisplayAllDetailsOfCharacters();
 		round = 1;
 		while (player->GetHealth() > 0 && enemy->GetHealth() > 0)
 		{
 			cout << "\n\nROUND : " << round << endl;
-			cout << endl << player->GetName() << "'s Turn choose your option\n";
-			PlayerGameRound();
-			Status();
-			cout << endl << endl << enemy->GetName() << "'s Turn\n";
-			EnemyGameRound();
-			Status();
+			run = CharacterTurnDecider();
+			if (run == 1)
+				return run;
 			round++;
 		}
+		return run;
+	}
+	int WizardAdventureMainMenu()
+	{
+		int secondoption;
+		int option;
+		while (true)
+		{
+			cout << "\nDO YOU WANT TO EXPLORE NEW WORLD OF FANTACY\n1. YES\n2. NO\nENTER YOUR CHOISE : ";
+			cin >> option;
+			switch (option)
+			{
+			case 1:
+
+				EnemyCharacterCreation();
+				run = PlayGame();
+				break;
+			case 2:
+				cout << "\nARE YOU SURE WANT TO GO BACK\n1. YES\n2. NO\nENTER YOUR CHOISE : ";
+				cin >> secondoption;
+				if (secondoption == 2)
+				{
+					EnemyCharacterCreation();
+					run = PlayGame();
+					break;
+				}
+				else if (secondoption == 1)
+				{
+					exit(0);
+				}
+				else
+				{
+					cout << "ENTERED OPTION IS NOT AVAILABLE SO CHOOSING DEFAULT OPTION NO";
+					exit(0);
+				}
+			default:
+				cout << "ENTERED OPTION IS NOT AVAILABLE SO CHOOSING DEFAULT OPTION NO";
+				break;
+			}
+		}
+
 	}
 public:
-	void Play()
+	void WizardAdventure()
 	{
 		//display game title
-		GameTitle();
+		DisplayGameTitle();
 		//character selection
-		CharacterCreation();
-		PlayGame();
+		PlayerCharacterCreation();
+		//DisplayStoryOfWizardAdventure();
+		WizardAdventureMainMenu();
+
 	}
 };
 
@@ -225,6 +304,6 @@ int main()
 {
 	//gameplay
 	GamePlay game;
-	game.Play();
+	game.WizardAdventure();
 	return 0;
 }
